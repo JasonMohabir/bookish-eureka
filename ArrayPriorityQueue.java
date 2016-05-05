3e57cc31544bbe96e981745502497bce1021b835
Jason Mohabir <jmohabir@stuy.edu>
2016-05-04 21:43:09 -0400

    MOAR MOAR

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,49 +1,108 @@
+//Team Bookish Eureka: Arpita Abrol, Jason Mohabir, William Xiang                                                                                                                    
+//APCS2 pd10                                                                                                                                                                         
+//HW35 -- Privileged Status Gets You to the Front of the Line                                                                                                                        
+//2016-05-04                                                                                                                                                                         
+
 import java.util.ArrayList;
 
-//Team Bookish Eureka: Arpita Abrol, Jason Mohabir, William Xiang
-//APCS2 pd10
-//HW35 -- Privileged Status Gets You to the Front of the Line  
-//2016-05-04 
+public class ArrayPriorityQueue<Comparable> implements PriorityQueue<Comparable> {
 
-public class ArrayPriorityQueue {
+    // instance vars
+    ArrayList<Comparable> items;
+    int size;
 
-    ArrayList items;
-
+    // constructor for class ArrayPriorityQueue
     public ArrayPriorityQueue() {
-        items = new ArrayList();
+        items = new ArrayList<Comparable>();
+        size = 0;
+    }
+
+    // postcondition: returns true if the number of elements in 
+    //                 the priority queue is 0; otherwise, returns false 
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    // postcondition: x has been added to the priority queue; 
+    //                 number of elements in the priority queue is increased by 1 
+    public void add( Comparable x ) {
+        items.add( (Comparable)x );
+        size++;
+    }
+
+    // postcondition: The smallest item in the priority queue is removed 
+    //                 and returned; the number of elements in the priority queue 
+    //                 is decreased by 1
+    //                 throws an uIndexOutOfBoundsException if the priority queue is empty 
+    public Comparable removeMin() {
+        int minIndex = findMin();
+        Comparable min = items.get( minIndex );
+        items.remove( minIndex );
+        size--;
+        return min;
+    }
+
+    // postcondition: The smallest item in the priority queue is returned; the 
+    //                 priority queue is unchanged 
+    //                 throws an IndexOutOfBoundsException if the priority queue is empty 
+    public Comparable peekMin() {
+        int minIndex = findMin();
+        Comparable min = items.get( minIndex );
+        return min;
     }
     
-    public boolean isEmpty(){
-        return items.size() == 0;
-    }
-
-    public void add( int x ) {
-        items.add(x);
-    }
-
-    public int removeMin() {
-        int min = peekMin();
-        items.remove(min);
-        return min; 
-    }
-
-    public int peekMin() {
-        int minIndex = 0;
-	for (int i = 1; i < items.size(); i++) //reigning champ algo
-            {
-                if ( (int)items.get(i) < (int)items.get(minIndex)) {minIndex = i;}
+    
+    // postcondition: The index of the smallest item in the priority queue  
+    //                 is returned; the priority queue is unchanged 
+    //                 throws an IndexOutOfBoundsException if the priority queue is empty 
+    public int findMin() {
+        if (size > 0) { //queue is empty
+            int minIndex = 0;
+            for (int i = 1; i < size; i++) { //reigning champ algo
+                Comparable min = items.get(minIndex);
+                Comparable itemAtIndex = items.get(i);
+                if ( itemAtIndex.compareTo( min ) < 0 )
+                    minIndex = i;
             }
-	return (int)items.get(minIndex);
+            return minIndex;
+        } else {
+            throw new IndexOutOfBoundsException("Error: Queue is empty!");
+        }
+    }
+    
+    public String toString() {
+        String retStr = "[ ";
+        for (int i = 0; i < size; i++) {
+            retStr += items.get(i) + ", ";
+        }
+        
+        if (size > 0) retStr = retStr.substring( 0, retStr.length() - 2 ) + " ]";
+        else retStr += "]";
+        
+        return retStr;
     }
 
     public static void main(String[] args) {
-        ArrayPriorityQueue tomo = new ArrayPriorityQueue();
+        
+        ArrayPriorityQueue<Integer> tomo = new ArrayPriorityQueue<Integer>();
+        
+        System.out.println("\n\nAdding elements to tomo...");
+        System.out.println("tomo before adding: " + tomo);
         tomo.add(1);
         tomo.add(5);
         tomo.add(7);
         tomo.add(2);
         tomo.add(5);
-        System.out.println(tomo.peekMin());
+        System.out.println("tomo after adding: " + tomo);
+        
+        
+        System.out.println("\n\nRemoving elements from tomo...");
+        while (true) { 
+            //should eventually result in IndexOutOfBoundsException being thrown
+            System.out.println("\nMin value: " + tomo.peekMin());
+            System.out.println(tomo.removeMin());
+            System.out.println("tomo after removing: " + tomo);
+        }
     }
 
 } //end class
--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,4 +1,9 @@
-public interface PriorityQueue 
+//Team Bookish Eureka: Arpita Abrol, Jason Mohabir, William Xiang                                                                                                                    
+//APCS2 pd10                                                                                                                                                                         
+//HW35 -- Privileged Status Gets You to the Front of the Line                                                                                                                        
+//2016-05-04 
+
+public interface PriorityQueue<Comparable>
 { 
     // postcondition: returns true if the number of elements in 
     //                 the priority queue is 0; otherwise, returns false 
@@ -6,16 +11,16 @@ public interface PriorityQueue
 
     // postcondition: x has been added to the priority queue; 
     //                 number of elements in the priority queue is increased by 1 
-    void add(Object x); 
+    void add(Comparable x); 
 
     // postcondition: The smallest item in the priority queue is removed 
     //                 and returned; the number of elements in the priority queue 
     //                 is decreased by 1
     //                 throws an unchecked exception if the priority queue is empty 
-    Object removeMin(); 
+    Comparable removeMin(); 
 
     // postcondition: The smallest item in the priority queue is returned; the 
     //                 priority queue is unchanged 
     //                 throws an unchecked exception if the priority queue is empty 
-    Object peekMin(); 
+    Comparable peekMin(); 
 } 
